d188dbbe5621f668cef5aad66c81527af5955bf1
Camilo Delgado <camilodc7@gmail.com>
2021-04-26 11:23:26 -0500

    name changes

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import './card.css';
+import React from "react";
+import "./card.css";
 
-const Card = props => (
-    <div className='card-container'>
-        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
-        <h4> {props.monster.name} </h4>
-        <p> {props.monster.email} </p>
-    </div>
+const Card = (props) => (
+  <div className="card-container">
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
+    <h4> {props.monster.name} </h4>
+    <p> {props.monster.email} </p>
+  </div>
 );
 
 export default Card;
